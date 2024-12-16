9ba8425f114c67abbc1485799f8db63bd50281b3
unknown <sanjeetkumarit@outlook.com>
2024-12-16 14:57:47 +0530

    added json-schema form component

--- src/components/LibraryComponents.tsx
+++ src/components/LibraryComponents.tsx
@@ -1,28 +1,31 @@
 //import * as React from 'react';
-import React from 'react';
-
+import React , {useRef,useEffect} from 'react';
+import { formSchema } from './../utils/form-schema'; // Import schema
+import {HTMLJsonSchemaFormElement} from './../structs/interfaces/json-schema.interface'
 //import { useNavigate,useParams } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
  function LibraryComponents() {
+    // Define the ref with the specific type for `json-schema-form`
+  // const formRef = useRef<any>(null);
+const formRef = useRef<HTMLJsonSchemaFormElement>(null);
 
+ // Use useEffect to set complex props
+ useEffect(() => {
+  if (formRef.current) {
+     // Safely set the schema property
+    formRef.current.schema = JSON.stringify(formSchema); // Pass schema directly
+  }
+}, []);
   return (
     <div>
     <h1>from 'stenciljs-components' npm package</h1>
-    <my-card user-name="CodingLocker"></my-card>
+     {/* Attach a typed ref */}
+    <json-schema-form 
+     ref={formRef} ></json-schema-form> 
     <my-pie-chart  data='[{"tag":"height","value":180},{"tag":"weight","value":75},{"tag":"age","value":30},{"tag":"score","value":95},{"tag":"yearsExperience","value":5}]'></my-pie-chart>
-    <my-rich-text-editor initial-value="this is Reaact value" placeholder="React placeholder"></my-rich-text-editor>
-    <my-progress-bar value="2" max="10"></my-progress-bar>
     <my-progress-ring percentage="30"></my-progress-ring>
-    <test-button button-id="test-button">Click me!</test-button>
-    <test-counter>Number: </test-counter>
-    <search-world search-text="bmw"> </search-world>
-    <my-payment-gateway></my-payment-gateway>
-    <my-component first="Sanjeet" last="Kumar"></my-component>
-    <simple-form></simple-form>
     <custom-form></custom-form>
-    <complex-ionic-form></complex-ionic-form>
-   <combo-box allow-input='true' label='Album:'></combo-box>
   
     </div>
   );
---
+++ src/structs/interfaces/json-schema.interface.ts
@@ -0,0 +1,3 @@
+export interface HTMLJsonSchemaFormElement extends HTMLElement {
+  schema: object | string; // Define the `schema` property
+}
